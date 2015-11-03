3cf6aa4520300c913d28c0d0b223b004396e0335
Ivo Bathke <ivo.bathke@gmail.com>
2015-11-03 14:09:00 +0100

    added docker-compose aliases

---
+++ aliases/docker-compose.aliases.bash
@@ -0,0 +1,8 @@
+cite about-alias
+about-alias 'docker-compose aliases'
+
+alias doco='docker-compose'
+alias docou='docker-compose up'
+alias docob='docker-compose build'
+alias docor='docker-compose run'
+alias docol='docker-compose logs'
